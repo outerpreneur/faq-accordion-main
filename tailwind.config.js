7388c5c815248406d8581e139e15a40ee086fc70
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-12-05 23:41:57 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        darkPurple: "#301534",
+        lightPurple: "#F8EEFF",
+      },
+    },
+  },
+  plugins: [],
+};
